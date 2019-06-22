bab3c2859d075adbc86585e1d7533aa4be5f7c2e
jackyxxxmax <qweasdjklzxc@gmail.com>
2019-06-22 13:58:40 +0800

    fully clear

--- cache.cpp
+++ cache.cpp
@@ -10,8 +10,8 @@ int main(int argc, char*argv[])
     int cache_size, block_size, asso_type, policy_type;
     int tag_width, index_width, offset_width;
     //test
-    //ifstream infile("trace2.txt", ios::in); //file open
-    //ofstream outfile("trace2.out", ios::out);
+    //ifstream infile("trace3.txt", ios::in); //file open
+    //ofstream outfile("trace3.out", ios::out);
     //test
     ifstream infile(argv[1], ios::in); //file open
     if(!infile)
@@ -57,10 +57,15 @@ int main(int argc, char*argv[])
     //and trace3.txt will core dumped
     
     //create cache using 2D-array
-	int start_index = 0;
 	int time_order = 0; //the time read in
 	int block_num = (cache_size*1024)/block_size;
     unsigned int cache[block_num][2];
+    int count = 0; //check data full for fully
+    int vicplace = 2100000000; //record the earliest time_order for fully
+    int cacheempty = 0;
+    int cachehit = 0;
+    int changeplace1 = 0;
+    int changeplace2 = 0;
     //initialize
     for(int i=0; i<block_num; i++)
 	{
@@ -79,13 +84,13 @@ int main(int argc, char*argv[])
 	{
 		while(infile>>std::hex>>target)
     	{
-    		temp = target / (int)pow(2, offset_width); //discard offset bits
-    		temp1 = temp % (int)pow(2, index_width); //get index bits
-    		temp2 = temp / (int)pow(2, index_width); //get tag bits
-    		temp = cache[temp1][0] / (int)pow(2, offset_width); //discard offset bits
-    		temp3 = temp / (int)pow(2, index_width); //get tag bits
     		if(asso_type == 0) //direct-mapped
 			{
+				temp = target / (int)pow(2, offset_width); //discard offset bits
+    			temp1 = temp % (int)pow(2, index_width); //get index bits
+    			temp2 = temp / (int)pow(2, index_width); //get tag bits
+    			temp = cache[temp1][0] / (int)pow(2, offset_width); //discard offset bits
+    			temp3 = temp / (int)pow(2, index_width); //get tag bits
 				if(cache[temp1][0] != target) //different address data
 				{
 					if(temp2 == temp3) //same index and same tag, Hit
@@ -93,7 +98,7 @@ int main(int argc, char*argv[])
 						outfile << "-1" << endl;
 						cache[temp1][0] = target; //update address data
 						time_order = time_order + 1;
-						cache[temp1][1] = time_order; //update time_order
+						cache[temp1][1] = time_order; //update time_order, maybe unnecessary
 						continue;
 					}
 					else //same index and different tag, Miss, select victim
@@ -108,7 +113,7 @@ int main(int argc, char*argv[])
 						}
 						cache[temp1][0] = target; //update address data
 						time_order = time_order + 1;
-						cache[temp1][1] = time_order; //update time_order
+						cache[temp1][1] = time_order; //update time_order, maybe unnecessary
 						continue;
 					}
 				}
@@ -116,7 +121,7 @@ int main(int argc, char*argv[])
 				{
 					outfile << "-1" << endl;
 					time_order = time_order + 1;
-					cache[temp1][1] = time_order; //update time_order
+					cache[temp1][1] = time_order; //update time_order, maybe unnecessary
 					continue;
 				}
 			}
@@ -125,6 +130,72 @@ int main(int argc, char*argv[])
 			}
 			else if(asso_type == 2) //fully
 			{
+				temp = target / (int)pow(2, offset_width); //discard offset bits and then
+				temp1 = temp; //get tag bits
+				for(int i=0; i<block_num; i++)
+				{
+					temp = cache[i][0] / (int)pow(2, offset_width); //discard offset bits and then
+					temp3 = temp; //get tag bits
+					if(cache[i][0] == target) //same address data, Hit
+					{
+						cachehit = 1;
+						outfile << "-1" << endl;
+						//Hit not need to update time_order
+						//time_order = time_order + 1;
+						//cache[i][1] = time_order; //update time_order
+						break;
+					}
+					else
+					{
+						if(temp3 == temp1){ //same tag, Hit
+							cachehit = 1;
+							outfile << "-1" << endl;
+							//Hit not need to update time_order
+							//time_order = time_order + 1;
+							//cache[i][1] = time_order; //update time_order
+							break;
+						}
+						else if((cache[i][0] != 0) & (cache[i][1] != 0)) //check if full then need to select victim
+						{
+							if(cache[i][1] < vicplace) //select victim place
+							{
+								vicplace = cache[i][1];
+								changeplace2 = i;
+							}
+							count = count + 1;
+						}
+						else if((cache[i][0] == 0) & (cache[i][1] == 0)) //cache has space to put data
+						{
+							cacheempty = 0;
+							changeplace1 = i; //select empty place
+						}
+					}
+				}
+				if(count == block_num) //cache is full, select victim
+				{
+					cacheempty = 1;
+					temp = cache[changeplace2][0] / (int)pow(2, offset_width); //discard offset bits and then
+					temp3 = temp; //get tag bits
+					outfile << temp3 << endl;
+					cache[changeplace2][0] = target; //update address data
+					time_order = time_order + 1;
+					cache[changeplace2][1] = time_order; //update time_order
+				}
+				else //cache not full
+				{
+					 if(cachehit != 1)
+					{
+						outfile << "-1" << endl;
+						cache[changeplace1][0] = target; //update address data
+						time_order = time_order + 1;
+						cache[changeplace1][1] = time_order; //update time_order
+					}
+				}
+				cachehit = 0;
+				vicplace = 2100000000;
+				count = 0;
+				changeplace1 = 0;
+				changeplace2 = 0;
 			}
     	}
 	}
@@ -132,13 +203,13 @@ int main(int argc, char*argv[])
 	{
 		while(infile>>std::hex>>target)
     	{
-    		temp = target / (int)pow(2, offset_width); //discard offset bits
-    		temp1 = temp % (int)pow(2, index_width); //get index bits
-    		temp2 = temp / (int)pow(2, index_width); //get tag bits
-    		temp = cache[temp1][0] / (int)pow(2, offset_width); //discard offset bits
-    		temp3 = temp / (int)pow(2, index_width); //get tag bits
     		if(asso_type == 0) //direct-mapped
 			{
+				temp = target / (int)pow(2, offset_width); //discard offset bits
+    			temp1 = temp % (int)pow(2, index_width); //get index bits
+    			temp2 = temp / (int)pow(2, index_width); //get tag bits
+    			temp = cache[temp1][0] / (int)pow(2, offset_width); //discard offset bits
+    			temp3 = temp / (int)pow(2, index_width); //get tag bits
 				if(cache[temp1][0] != target) //different address data
 				{
 					if(temp2 == temp3) //same index and same tag, Hit
@@ -146,7 +217,7 @@ int main(int argc, char*argv[])
 						outfile << "-1" << endl;
 						cache[temp1][0] = target; //update address data
 						time_order = time_order + 1;
-						cache[temp1][1] = time_order; //update time_order
+						cache[temp1][1] = time_order; //update time_order, maybe unnecessary
 						continue;
 					}
 					else //same index and different tag, Miss, select victim
@@ -161,7 +232,7 @@ int main(int argc, char*argv[])
 						}
 						cache[temp1][0] = target; //update address data
 						time_order = time_order + 1;
-						cache[temp1][1] = time_order; //update time_order
+						cache[temp1][1] = time_order; //update time_order, maybe unnecessary
 						continue;
 					}
 				}
@@ -169,7 +240,7 @@ int main(int argc, char*argv[])
 				{
 					outfile << "-1" << endl;
 					time_order = time_order + 1;
-					cache[temp1][1] = time_order; //update time_order
+					cache[temp1][1] = time_order; //update time_order, maybe unnecessary
 					continue;
 				}
 			}
@@ -178,6 +249,70 @@ int main(int argc, char*argv[])
 			}
 			else if(asso_type == 2) //fully
 			{
+				temp = target / (int)pow(2, offset_width); //discard offset bits and then
+				temp1 = temp; //get tag bits
+				for(int i=0; i<block_num; i++)
+				{
+					temp = cache[i][0] / (int)pow(2, offset_width); //discard offset bits and then
+					temp3 = temp; //get tag bits
+					if(cache[i][0] == target) //same address data, Hit
+					{
+						cachehit = 1;
+						outfile << "-1" << endl;
+						time_order = time_order + 1;
+						cache[i][1] = time_order; //update time_order
+						break;
+					}
+					else
+					{
+						if(temp3 == temp1){ //same tag, Hit
+							cachehit = 1;
+							outfile << "-1" << endl;
+							time_order = time_order + 1;
+							cache[i][1] = time_order; //update time_order
+							break;
+						}
+						else if((cache[i][0] != 0) & (cache[i][1] != 0)) //check if full then need to select victim
+						{
+							if(cache[i][1] < vicplace) //select victim place
+							{
+								vicplace = cache[i][1];
+								changeplace2 = i;
+							}
+							count = count + 1;
+						}
+						else if((cache[i][0] == 0) & (cache[i][1] == 0)) //cache has space to put data
+						{
+							cacheempty = 0;
+							changeplace1 = i; //select empty place
+						}
+					}
+				}
+				if(count == block_num) //cache is full, select victim
+				{
+					cacheempty = 1;
+					temp = cache[changeplace2][0] / (int)pow(2, offset_width); //discard offset bits and then
+					temp3 = temp; //get tag bits
+					outfile << temp3 << endl;
+					cache[changeplace2][0] = target; //update address data
+					time_order = time_order + 1;
+					cache[changeplace2][1] = time_order; //update time_order
+				}
+				else //cache not full
+				{
+					 if(cachehit != 1)
+					{
+						outfile << "-1" << endl;
+						cache[changeplace1][0] = target; //update address data
+						time_order = time_order + 1;
+						cache[changeplace1][1] = time_order; //update time_order
+					}
+				}
+				cachehit = 0;
+				vicplace = 2100000000;
+				count = 0;
+				changeplace1 = 0;
+				changeplace2 = 0;
 			}
     	}
 	}
@@ -185,13 +320,13 @@ int main(int argc, char*argv[])
 	{
 		while(infile>>std::hex>>target)
     	{
-    		temp = target / (int)pow(2, offset_width); //discard offset bits
-    		temp1 = temp % (int)pow(2, index_width); //get index bits
-    		temp2 = temp / (int)pow(2, index_width); //get tag bits
-    		temp = cache[temp1][0] / (int)pow(2, offset_width); //discard offset bits
-    		temp3 = temp / (int)pow(2, index_width); //get tag bits
     		if(asso_type == 0) //direct-mapped
 			{
+				temp = target / (int)pow(2, offset_width); //discard offset bits
+    			temp1 = temp % (int)pow(2, index_width); //get index bits
+    			temp2 = temp / (int)pow(2, index_width); //get tag bits
+    			temp = cache[temp1][0] / (int)pow(2, offset_width); //discard offset bits
+    			temp3 = temp / (int)pow(2, index_width); //get tag bits
 				if(cache[temp1][0] != target) //different address data
 				{
 					if(temp2 == temp3) //same index and same tag, Hit
@@ -199,7 +334,7 @@ int main(int argc, char*argv[])
 						outfile << "-1" << endl;
 						cache[temp1][0] = target; //update address data
 						time_order = time_order + 1;
-						cache[temp1][1] = time_order; //update time_order
+						cache[temp1][1] = time_order; //update time_order, maybe unnecessary
 						continue;
 					}
 					else //same index and different tag, Miss, select victim
@@ -214,7 +349,7 @@ int main(int argc, char*argv[])
 						}
 						cache[temp1][0] = target; //update address data
 						time_order = time_order + 1;
-						cache[temp1][1] = time_order; //update time_order
+						cache[temp1][1] = time_order; //update time_order, maybe unnecessary
 						continue;
 					}
 				}
@@ -222,7 +357,7 @@ int main(int argc, char*argv[])
 				{
 					outfile << "-1" << endl;
 					time_order = time_order + 1;
-					cache[temp1][1] = time_order; //update time_order
+					cache[temp1][1] = time_order; //update time_order, maybe unnecessary
 					continue;
 				}
 			}
